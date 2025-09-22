b5cef240f0ce14d4fe35b5d3370feb6c8c5ee001
Hilton Nery <hilton.nery@indep.local>
2025-09-22 16:57:30 -0300

    bot on

--- bot.py
+++ bot.py
@@ -4,3 +4,20 @@ import asyncpg
 from dotenv import load_dotenv
 import os
 from openai import OpenAI
+
+load_dotenv() 
+token = os.getenv("DISCORD_TOKEN")
+print(token)  
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.command()
+async def ping(contexto):
+    print(contexto.author)
+    print(contexto.channel)
+    await contexto.send("Pong!")
+    
+bot.run(token)
